ed575656a59a6bd1a7c952fb2d354dbf4013c498
Jeeva <jeevai1806@gmail.com>
2026-08-03 16:06:13 +0000

    Initial commit: Emertxe Address Book project

---
+++ AddressBook-NewDesign/AddressBook-NewDesign/contact.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "contact.h"
+#include "file.h"
+#include "populate.h"
+
+void listContacts(AddressBook *addressBook, int sortCriteria) 
+{
+    // Sort contacts based on the chosen criteria
+    
+}
+
+void initialize(AddressBook *addressBook) {
+    addressBook->contactCount = 0;
+    populateAddressBook(addressBook);
+    
+    // Load contacts from file during initialization (After files)
+    //loadContactsFromFile(addressBook);
+}
+
+void saveAndExit(AddressBook *addressBook) {
+    saveContactsToFile(addressBook); // Save contacts to file
+    exit(EXIT_SUCCESS); // Exit the program
+}
+
+
+void createContact(AddressBook *addressBook)
+{
+	/* Define the logic to create a Contacts */
+    
+}
+
+void searchContact(AddressBook *addressBook) 
+{
+    /* Define the logic for search */
+}
+
+void editContact(AddressBook *addressBook)
+{
+	/* Define the logic for Editcontact */
+    
+}
+
+void deleteContact(AddressBook *addressBook)
+{
+	/* Define the logic for deletecontact */
+   
+}
